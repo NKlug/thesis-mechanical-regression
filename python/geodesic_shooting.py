1090f856fd931931aba213f284aa2cba3b99ccb6
Nikolas Klug <klug.nikolas@googlemail.com>
2020-11-25 15:46:31 +0100

    Properly calculate gradient of V

--- python/geodesic_shooting.py
+++ python/geodesic_shooting.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from derivatives import symmetric_difference_quotient
 from kernels import K_block, gamma
 from leapfrog import explicit_leapfrog
 from losses import optimal_recovery_loss
@@ -18,23 +19,7 @@ def V(p0, X, Y, mu=0.01):
     :return: Value of V in (3.17)
     """
     q, _ = explicit_leapfrog(ham.dq_h, ham.dp_h, X, p0, step=0.001)
-    return mu / 2 * vector_vector_mult(vector_matrices_mult(p0, gamma(X, X)), p0) + \
-           optimal_recovery_loss(q[-1], Y, K_block)
-
-
-def derivative_V(p0, direction, X, Y, h=0.001):
-    """
-    Calculates the symmetric difference quotient of V in the given direction
-    :param h: parameter for the symmetric difference quotient
-    :param p0: point at which to calculate the derivative
-    :param direction: derivation direction
-    :param X: Training data
-    :param Y: Training labels
-    :return: The derivative of V in the given direction
-    """
-    direction = np.asarray(direction)
-    delta_h = np.tile(h * direction, reps=(p0.shape[0], 1))
-    return (V(p0 - delta_h, X, Y) - V(p0 + delta_h, X, Y)) / (2 * h)
+    return mu / 2 * p0.T @ gamma(X, X) @ p0 + optimal_recovery_loss(q[-1], Y, K_block)
 
 
 def grad_V(p0, X, Y):
@@ -45,6 +30,16 @@ def grad_V(p0, X, Y):
     :param Y: Training labels
     :return: The gradient of V at point p0
     """
-    x = derivative_V(p0, [1, 0], X, Y)
-    y = derivative_V(p0, [0, 1], X, Y)
-    return np.stack([x, y], axis=-1)
+
+    def partial_V(direction):
+        """
+
+        :param direction: index of direction in which to calculate the partial derivative
+        :return:
+        """
+        dir_vector = np.zeros(p0.shape[0])
+        dir_vector[direction] = 1
+        f = lambda x: V(p0 + dir_vector * x, X, Y)
+        return symmetric_difference_quotient(f, 0)
+
+    return np.vectorize(partial_V)(np.arange(p0.shape[0]))
