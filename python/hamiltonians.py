65692970fde89dfda2651a7126e678d4ad83bf42
Nikolas Klug <klug.nikolas@googlemail.com>
2020-11-25 15:46:59 +0100

    Adjust hamiltonians to work with new notation

--- python/hamiltonians.py
+++ python/hamiltonians.py
@@ -1,10 +1,12 @@
+from derivatives import symmetric_difference_quotient
 from kernels import gamma
 from multiplication_helpers import vector_matrices_mult
 
 
 def dq_h(q, p):
-    return 0
+    f = lambda q1: 0.5 * p.T @ gamma(q1, q1) @ p
+    return symmetric_difference_quotient(f, q)
 
 
 def dp_h(q, p):
-    return vector_matrices_mult(p, gamma(q, q))
+    return p @ gamma(q, q)
