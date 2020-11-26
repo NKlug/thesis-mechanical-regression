Fix derivatives and add cython kernels

python/derivatives.py
@@ -9,8 +9,8 @@ def symmetric_difference_quotient(f, x, h=None):
     :param x: the point
     :return:
     """
-    # TODO ensure we use float64 everywhere
+    x = np.asarray(x)
     if h is None:
         h = np.cbrt(np.finfo(np.float32).eps)  # https://en.wikipedia.org/wiki/Numerical_differentiation#Step_size
-    h = np.tile(h, reps=(x.shape[:-1], 1))
-    return (f(x - h) + f(x + h)) / (2 * h)
+    h = np.tile(h, reps=x.shape)
+    return (f(x + h) - f(x - h)) / (2 * h)

python/gradient_descent.py
@@ -10,7 +10,7 @@ def gradient_descent(grad_f, start, learning_rate, steps):
     return solutions
 
 
-def adam(grad_f, start, X, Y, steps, alpha=0.001, beta_1=0.9, beta_2=0.999, epsilon=10e-8):
+def adam(grad_f, start, X, Y, steps, alpha=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-8, callback=None):
     solutions = np.zeros((steps, *start.shape))
     solutions[0] = start
     m = 0
@@ -22,4 +22,6 @@ def adam(grad_f, start, X, Y, steps, alpha=0.001, beta_1=0.9, beta_2=0.999, epsi
         m = m / (1 - np.power(beta_1, t))
         v = v / (1 - np.power(beta_2, t))
         solutions[t] = solutions[t - 1] - alpha * m / (np.sqrt(v) + epsilon)
+        if callback:
+            callback(solutions[t], X, Y)
     return solutions

python/leapfrog.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tqdm import tqdm
 
 
 def explicit_leapfrog(dq_h, dp_h, q0, p0, step, t_stop=1):
@@ -13,14 +14,16 @@ def explicit_leapfrog(dq_h, dp_h, q0, p0, step, t_stop=1):
     :param dp_h: partial derivative of Hamiltonian w.r.t p
     :return: flows q and p
     """
+    q0 = np.asarray(q0)
+    p0 = np.asarray(p0)
     num_steps = int(1 / step) * t_stop
     q = np.zeros((num_steps, *q0.shape))
     p = np.zeros((num_steps, *p0.shape))
     q[0] = q0
     p[0] = p0
     for i in range(1, num_steps):
-        curr_q = q[i-1]
-        curr_p = p[i-1]
+        curr_q = q[i - 1]
+        curr_p = p[i - 1]
         curr_p = curr_p - step / 2 * dq_h(curr_q, curr_p)
         curr_q = curr_q + step * dp_h(curr_q, curr_p)
         curr_p = curr_p - step / 2 * dq_h(curr_q, curr_p)