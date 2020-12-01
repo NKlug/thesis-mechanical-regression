ae2774218428fef98651cec4e34ae48590e02bc0
Nikolas Klug <klug.nikolas@googlemail.com>
2020-12-02 00:20:15 +0100

    Limit gpu memory usage

--- python/run.py
+++ python/run.py
@@ -4,7 +4,9 @@ import tensorflow as tf
 
 if __name__ == '__main__':
     # limit gpu memory allocation
-    tf.config.experimental.set_memory_growth(tf.config.list_physical_devices('GPU')[0], True)
+    gpus = tf.config.list_physical_devices('GPU')
+    if len(gpus) > 0:
+        tf.config.experimental.set_memory_growth(gpus[0], True)
     # Create Dataset: X is (2N x 1) and Y is (N x 1)
     X, Y = generate_swiss_roll_dataset(1, jitter=0.1, coils=0.65, n=40)
 
